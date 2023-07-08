2bded1afad21b89f56e158a4dbed4f711320bbd9
Agustin Mista <agustin@mista.me>
2023-07-08 23:37:45 +0000

    Add action to build and deploy to Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/qmk-rgb-live/',
   plugins: [
     vue(),
     vueJsx(),
